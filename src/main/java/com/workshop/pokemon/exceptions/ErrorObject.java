a7ae6d491280f120a4f067ba4e54d5ad81ef5741
Giga Amiridze <giga.amiridze@azry.com>
2024-01-01 16:16:04 +0400

    Added global exception handler

---
+++ src/main/java/com/workshop/pokemon/exceptions/ErrorObject.java
@@ -0,0 +1,50 @@
+package com.workshop.pokemon.exceptions;
+
+import java.util.Date;
+
+public class ErrorObject {
+    private int statusCode;
+    private String message;
+    private Date timestamp;
+
+    public ErrorObject() {}
+
+    public ErrorObject(int statusCode, String message, Date timestamp) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorObject{" +
+                "statusCode=" + statusCode +
+                ", message='" + message + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
