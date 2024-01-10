ea1914b583d3c823bb38b6d096be303ea473a7df
Giga Amiridze <giga.amiridze@azry.com>
2024-01-10 10:31:40 +0400

    Added review controller

---
+++ src/main/java/com/workshop/pokemon/controllers/ReviewController.java
@@ -0,0 +1,25 @@
+package com.workshop.pokemon.controllers;
+
+import com.workshop.pokemon.dto.ApiResponse;
+import com.workshop.pokemon.dto.ReviewDto;
+import com.workshop.pokemon.services.impl.ReviewServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/review")
+public class ReviewController {
+
+    private final ReviewServiceImpl reviewService;
+
+    @Autowired
+    public ReviewController(ReviewServiceImpl reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping("/{pokemonId}")
+    public ResponseEntity<ApiResponse> createReview(@PathVariable Long pokemonId, @RequestBody ReviewDto reviewDto) {
+        return reviewService.createReview(pokemonId, reviewDto);
+    }
+}
